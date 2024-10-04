3c0cc6f841a63fd1a6c88d01b71215eb33e38375
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-10-04 18:12:53 +0200

    add role component

--- frontend/src/app/components/user-backend/user-backend.component.ts
+++ frontend/src/app/components/user-backend/user-backend.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { UserBackendService } from '../../../services/user/user-backend.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { RoleComponent } from '../role/role.component';
 
 @Component({
   selector: 'app-user-backend',
   standalone: true,
-  imports: [],
+  imports: [RoleComponent],
   templateUrl: './user-backend.component.html',
   styleUrl: './user-backend.component.scss',
 })
--- frontend/src/app/components/user-keycloak/user-keycloak.component.ts
+++ frontend/src/app/components/user-keycloak/user-keycloak.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { SecurityStore } from '../../../store/security-store';
+import { RoleComponent } from '../role/role.component';
 
 @Component({
   selector: 'app-user-keycloak',
   standalone: true,
-  imports: [],
+  imports: [RoleComponent],
   templateUrl: './user-keycloak.component.html',
   styleUrl: './user-keycloak.component.scss',
 })
